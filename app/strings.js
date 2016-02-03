af3aa787d7fcc11d207e2d4eb08516b782d31618
Hitesh Kumar <hk583366@gmail.com>
2016-02-02 23:00:24 -0500

    Update strings.js

--- app/strings.js
+++ app/strings.js
@@ -1,15 +1,45 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.stringsAnswers = {
-    reduceString: function (str, amount) {
-    },
-    wordWrap: function (str, cols) {
+  reduceString: function(str, amount) {
 
-    },
-    reverseString: function (str) {
-        var output = '';
-        for (var i = str.length - 1; i >= 0; i--)
-            output += str[i];
-        return output;
+    var len = str.length,
+      char,
+      charCount = {},
+      newStr = [];
+
+    for (var i = 0; i < len; i++) {
+      char = str[i];
+
+      if (charCount[char]) {
+        charCount[char]++;
+      } else {
+        charCount[char] = 1;
+      }
     }
+    for (var key in charCount) {
+      if (charCount.hasOwnProperty(key)) {
+        var inc = 0;
+        // if char count =1 add it new array
+        if (charCount[key] === 1) {
+          newStr.push(key);
+        } else {
+          while (inc < amount && charCount[key] > amount) {
+            newStr.push(key);
+            inc++;
+          }
+        }
+      }
+    }
+    return (newStr.join(''));
+  },
+  wordWrap: function(str, cols) {
+
+  },
+  reverseString: function(str) {
+    var output = '';
+    for (var i = str.length - 1; i >= 0; i--)
+      output += str[i];
+    return output;
+  }
 };
